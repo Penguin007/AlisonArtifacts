8de78302e6ee4c37597aadad906fd4cf703c4664
Matt Guinn <mguinn0978@gmail.com>
2020-03-29 12:51:44 -0500

    Update

--- ArtifactsDatamodel/Models/Owner.cs
+++ ArtifactsDatamodel/Models/Owner.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace ArtifactsDatamodel.Models
 {
+    /// <summary>
+    /// The owner of the artifact
+    /// </summary>
     public class Owner
     {
         public int Id { get; set; }
